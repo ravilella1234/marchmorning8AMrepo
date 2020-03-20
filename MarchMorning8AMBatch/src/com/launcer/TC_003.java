7d59368226fdc36ced3932cb7e127242851bd2c7
maha <maha@gmail.com>
2020-03-20 08:59:39 +0530

    Browser Profile creation for chrome & firefox

---
+++ MarchMorning8AMBatch/src/com/launcer/TC_003.java
@@ -0,0 +1,16 @@
+package com.launcer;
+
+public class TC_003 extends BaseTest
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		init();
+		
+		openBrowser("chromebrowser");
+		
+		navigateUrl("iciciurl");
+
+	}
+
+}

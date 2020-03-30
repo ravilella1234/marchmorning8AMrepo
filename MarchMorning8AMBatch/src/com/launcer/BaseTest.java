80b54eaef9720eda56c7a0b37e6a2438eb266d0a
maha <maha@gmail.com>
2020-03-30 08:58:50 +0530

    log4j

--- MarchMorning8AMBatch/src/com/launcer/BaseTest.java
+++ MarchMorning8AMBatch/src/com/launcer/BaseTest.java
@@ -3,10 +3,12 @@ package com.launcer;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -39,6 +41,9 @@ public class BaseTest
 		subprop.load(fis);
 		String url = subprop.getProperty("amazonurl");
 		System.out.println(url);
+		
+		fis=new FileInputStream(projectpath+"//log4jconfig.properties");
+		PropertyConfigurator.configure(fis);
 	}
 	
 	
@@ -47,6 +52,8 @@ public class BaseTest
 		if(p.getProperty(browser).equals("chrome"))
 		{
 			System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\driver sources\\chromedriver.exe");
+			System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "false");
+			
 			
 			ChromeOptions option=new ChromeOptions();
 			option.addArguments("user-data-dir=C:\\Users\\DELL\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 7");
--- MarchMorning8AMBatch/src/com/launcer/TC_003.java
+++ MarchMorning8AMBatch/src/com/launcer/TC_003.java
@@ -1,28 +1,33 @@
 package com.launcer;
 
+import org.apache.log4j.Logger;
+
 public class TC_003 extends BaseTest
 {
 
+	private static final Logger log=Logger.getLogger(TC_003.class);
+	
+	
 	public static void main(String[] args) throws Exception 
 	{
 		init();
+		log.info("Initializing Properties files......");
 		
 		openBrowser("chromebrowser");
+		log.info("Opened the browser :- " + p.getProperty("chromebrowser"));
 		
 		navigateUrl("amazonurl");
+		log.info("Navigated to url :- " + subprop.getProperty("amazonurl"));
 		
 		selectOption("amazondropdown_id","amazondropvalue");
+		log.info("Selected the option :- " + mainprop.getProperty("amazondropvalue") + " by using locator :- " + mainprop.getProperty("amazondropdown_id") );
 			
 		type("amazonsearchtext_id","amzonsearchtext");
+		log.info("Entered the text :- "+ mainprop.getProperty("amzonsearchtext") + " by using locator :- " + mainprop.getProperty("amazonsearchtext_id"));
 		
 		clickElement("amazonsearchbutton_xpath");
+		log.info("Clicked on element by using the locator :- " + mainprop.getProperty("amazonsearchbutton_xpath"));
 		
-		//driver.findElement(By.id("searchDropdownBox")).sendKeys("Books");
-		
-		//driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Harry Potter");
-		
-		//driver.findElement(By.xpath("//div[@class='nav-search-submit nav-sprite']//input[@class='nav-input']")).click();
-
 	}
 
 }

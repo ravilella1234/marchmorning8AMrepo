5b57f52900d649c4f5444b550c760cc2666f945d
maha <maha@gmail.com>
2020-04-09 09:12:52 +0530

    Addded Ecommerce Maven project into Github

---
+++ Ecommerce/src/test/java/com/project/Ecommerce/CheckBox1.java
@@ -0,0 +1,55 @@
+package com.project.Ecommerce;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+public class CheckBox1 extends BaseTest
+{
+  
+	@BeforeMethod
+	@Parameters("browser")
+	  public void beforeMethod(String bType) throws Exception 
+	  {
+		init();
+		test=report.startTest("CheckBox1");
+		test.log(LogStatus.INFO, "Initializing the Properties Files....");
+		
+		openBrowser(bType);
+		test.log(LogStatus.INFO, "Opened the browser :-" + p.getProperty("chromebrowser"));
+		
+		navigateUrl("checkboxurl");
+		test.log(LogStatus.INFO, "Navigated to url :-" + subprop.getProperty("checkboxurl"));
+		
+	  }
+	  
+	  
+	@Test
+	public void checkboxTest()
+	{
+		List<WebElement> check=driver.findElements(By.xpath("//td[@class='table5']/input[@type='checkbox']"));
+		for(int i=0;i<check.size();i++)
+		{
+			System.out.println(check.get(i).getAttribute("value")+"--"+check.get(i).getAttribute("checked"));
+			//System.out.println(check.get(i).isSelected());
+		}
+	}
+ 
+
+  @AfterMethod
+  public void afterMethod() 
+  {
+	 driver.quit();
+	 
+	 report.endTest(test);
+	 report.flush();
+  }
+
+}

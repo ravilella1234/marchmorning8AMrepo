a8c9559e960d6f08ced84f0da54cd8694a4ffdee
maha <maha@gmail.com>
2020-05-27 09:00:55 +0530

    Prepared TestData for testcases

---
+++ CRMProject/src/test/java/com/project/SuiteA/LoginTest.java
@@ -0,0 +1,9 @@
+package com.project.SuiteA;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+  @Test
+  public void f() {
+  }
+}
---
+++ CRMProject/src/test/java/com/project/SuiteA/TestA.java
@@ -0,0 +1,9 @@
+package com.project.SuiteA;
+
+import org.testng.annotations.Test;
+
+public class TestA {
+  @Test
+  public void f() {
+  }
+}
